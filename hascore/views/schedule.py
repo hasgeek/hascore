a6bd1f5c42cd99825de258b9691921e26a59bc4f
Kiran Jonnalagadda <kiran@hasgeek.com>
2012-11-03 00:32:24 +0530

    Data structure changes.

--- hascore/views/schedule.py
+++ hascore/views/schedule.py
@@ -1,4 +1,5 @@
 from datetime import datetime, date
+from flask import url_for
 from coaster.views import jsonp, requestargs
 from hascore import app
 
@@ -16,30 +17,36 @@ def events_list():
                 'buid': 'off7nXRQQ7-S1ZK-D_tIdQ',
                 },
             },
-        'workspaces': [{
-            'profile_id': 'off7nXRQQ7-S1ZK-D_tIdQ',
-            'name': '2012',
-            'title': 'Droidcon India 2012',
-            'short_title': '2012',
-            'flags': ['funnel', 'schedule'],
-            'features': {
-                'artwork': {
-                    'color_bg': '#ffffff',
-                    'color_bg2': '#ffffff',
-                    'color_fg': '#222',
-                    'color_fg2': '#222',
-                    'logo_url': None,
-                    'cover_url': None
-                    },
-                'schedule': {
-                    'date_location': '2 & 3 November, Bangalore',
-                    'start_date': date(2012, 11, 2).isoformat(),
-                    'end_date': date(2012, 11, 3).isoformat(),
-                    'start': datetime(2012, 11, 2, 3, 30, 0).isoformat() + 'Z',
-                    'end': datetime(2012, 11, 3, 12, 30, 0).isoformat() + 'Z',
+        'workspaces': {
+            'Mg8vrAAlSIanvE3qsZu2Dg': {
+                'profile_id': 'off7nXRQQ7-S1ZK-D_tIdQ',
+                'buid': 'Mg8vrAAlSIanvE3qsZu2Dg',
+                'name': '2012',
+                'title': 'Droidcon India 2012',
+                'short_title': '2012',
+                'feature_flags': ['funnel', 'schedule'],
+                'features': {
+                    'artwork': {
+                        'color_bg': '#ffffff',
+                        'color_bg2': '#ffffff',
+                        'color_fg': '#222',
+                        'color_fg2': '#222',
+                        'logo_url': None,
+                        'cover_url': None
+                        },
+                    'schedule': {
+                        'date_location': '2 & 3 November, Bangalore',
+                        'start_date': date(2012, 11, 2).isoformat(),
+                        'end_date': date(2012, 11, 3).isoformat(),
+                        'start': datetime(2012, 11, 2, 3, 30, 0).isoformat() + 'Z',
+                        'end': datetime(2012, 11, 3, 12, 30, 0).isoformat() + 'Z',
+                        'data_api_url': url_for('schedule_list',
+                            profile='off7nXRQQ7-S1ZK-D_tIdQ', workspace='Mg8vrAAlSIanvE3qsZu2Dg',
+                            _external=True)
+                        }
                     }
                 }
-            }]
+            }
         })
 
 
