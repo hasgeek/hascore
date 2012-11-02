cc7f47d372cc8ad7ec726a7535c2c301214b4231
Kiran Jonnalagadda <kiran@hasgeek.com>
2012-11-02 23:33:36 +0530

    Static data for list of events.

--- hascore/views/__init__.py
+++ hascore/views/__init__.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
 
-import os
-from flask import send_from_directory
-from hascore import app
-
 import hascore.views.login
+import hascore.views.schedule
--- hascore/views/schedule.py
+++ hascore/views/schedule.py
@@ -1,34 +1,49 @@
+from datetime import datetime, date
+from coaster.views import jsonp
 from hascore import app
 
 
-@app.route('/1/events')
-def eventseries_list():
+@app.route('/1/events/get')
+def events_list():
     """
-    List all event series (limited to those with upcoming editions?).
+    List all upcoming events.
     """
-    pass
+    return jsonp({
+        'profiles': [{
+            'name': 'droidconin',
+            'title': 'Droidcon India',
+            'buid': 'off7nXRQQ7-S1ZK-D_tIdQ',
+            }],
+        'workspaces': [{
+            'profile_id': 'off7nXRQQ7-S1ZK-D_tIdQ',
+            'name': '2012',
+            'title': 'Droidcon India 2012',
+            'short_title': '2012',
+            'flags': ['funnel', 'schedule'],
+            'features': {
+                'artwork': {
+                    'color_bg': '#ffffff',
+                    'color_bg2': '#ffffff',
+                    'color_fg': '#222',
+                    'color_fg2': '#222',
+                    'logo_url': None,
+                    'cover_url': None
+                    },
+                'schedule': {
+                    'date_location': '2 & 3 November, Bangalore',
+                    'start_date': date(2012, 11, 2).isoformat(),
+                    'end_date': date(2012, 11, 3).isoformat(),
+                    'start': None,
+                    'end': None,
+                    }
+                }
+            }]
+        })
 
 
-@app.route('/1/events/<eventseries>')
-def eventeditions_list(eventseries):
+@app.route('/1/schedules/get')
+def eventeditions_list():
     """
-    List all (upcoming?) editions of an event.
-    """
-    pass
-
-
-@app.route('/1/events/<eventseries>/editions/<edition>')
-def eventedition_info():
-    """
-    List of rooms, slots and sessions in a given event edition.
-    """
-    pass
-
-
-# Ugh! Make this /1/sessions/<sessionid>
-@app.route('/1/events/<eventseries>/editions/<edition>/sessions/<session>')
-def session_info():
-    """
-    Detail on a given session at an event.
+    List schedules for requested event.
     """
     pass
