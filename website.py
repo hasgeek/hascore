da23e18f9da3bfa254ddaa24f301e83198da66a2
Rudi MK <rudimk.firefly@gmail.com>
2014-11-11 13:09:29 +0000

    Imported views from hascore.

--- website.py
+++ website.py
@@ -1,8 +1,7 @@
 import sys
 import os.path
-import views
 sys.path.insert(0, os.path.dirname(__file__))
-from hascore import app as application, init_for
+from hascore import app as application, init_for, views
 init_for('production')
 with app.test_request_context():
 	views.networkbar.cache_networkbar_links()
