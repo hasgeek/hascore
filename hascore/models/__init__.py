6b938313db3a16acd6796c8c3f716683de539811
Shreyas Satish <shreyas.satish@gmail.com>
2017-05-05 12:03:48 +0530

    flask-alembic -> flask-migrate

--- hascore/models/__init__.py
+++ hascore/models/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from .. import app
 from coaster.sqlalchemy import IdMixin, TimestampMixin, BaseMixin, BaseNameMixin
 
-db = SQLAlchemy(app)
+from coaster.db import db
 
 from .user import *
 from .networkbar import *
